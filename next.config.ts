f36d3d15a130d33f4bc737ad6eaeb7b3c67ef0ad
ashik <ashikex49@gmail.com>
2025-10-05 11:21:58 +0600

    fff

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/middleware-manifest\.json$/],
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    // Optional Next 15 experimental flags here
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
